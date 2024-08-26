2195c9e68fc039ff3e3eb53f875cf8b5b511ea29
mycodedlife <ifiokusanga69@gmail.com>
2024-08-26 01:43:02 +0100

    store-setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import '/index.css'
+import './store'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "./index.js";
+
+const store = createStore({
+    state:{},
+    getters:{},
+    actions:{},
+    mutation:{},
+})
+
+export default store
